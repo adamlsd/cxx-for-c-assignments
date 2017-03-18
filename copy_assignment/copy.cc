6d5297386c86091024521d9bb8b584a73ff42c5c
ADAM David Alan Martin <adamartin@FreeBSD.org>
2017-03-17 23:13:00 -0400

    Add a buffer-aware set of read/write methods to the `FileGuard`.
    This makes the actual copy program code much more straightforward.

--- copy_assignment/copy.cc
+++ copy_assignment/copy.cc
@@ -20,64 +20,6 @@ const size_t COPYBUFSIZE= ( 256 * 1024 );
 
 const bool DEBUG_MODE= true;
 
-// File guard could be reference counted, to make a handy file-opening
-// function which prints errors... and the guard can have an "error"
-// state function.
-struct FileGuard
-{
-	FILE *fp;
-	size_t *refcount;
-
-	FileGuard()
-	{
-		refcount= new size_t( 1 );
-		fp= 0;
-	}
-
-	FileGuard( const char *name, const char *mode )
-	{
-		refcount= new size_t( 1 );
-		fp= fopen( name, mode );
-	}
-
-	FileGuard( const FileGuard &copy )
-	{
-		fp= copy.fp;
-		refcount= copy.refcount;
-		++*refcount;
-	}
-
-	FileGuard &operator= ( const FileGuard &copy )
-	{
-		if( !--*refcount )
-		{
-			fclose( fp );
-			delete refcount;
-		}
-		fp= copy.fp;
-		refcount= copy.refcount;
-		++*refcount;
-
-		return *this;
-	}
-
-	~FileGuard()
-	{
-		if( !--*refcount )
-		{
-			fclose( fp );
-			delete refcount;
-		}
-	}
-
-	// Returns true if there is no file open, or
-	// if there is an error on the file pointer itself.
-	bool error() { return !fp || ferror( fp ); }
-
-	// Returns true when a file is at the end (or no file is open.)
-	bool eof() { return !fp || feof( fp ); }
-};
-
 // MemoryGuard could be modified, optionally
 // to implement a reference-counting semantic
 // to help with lifetime issues.
@@ -168,6 +110,87 @@ struct MemoryGuard
 	}
 };
 
+// File guard could be reference counted, to make a handy file-opening
+// function which prints errors... and the guard can have an "error"
+// state function.
+struct FileGuard
+{
+	FILE *fp;
+	size_t *refcount;
+
+	FileGuard()
+	{
+		refcount= new size_t( 1 );
+		fp= 0;
+	}
+
+	FileGuard( const char *name, const char *mode )
+	{
+		refcount= new size_t( 1 );
+		fp= fopen( name, mode );
+	}
+
+	FileGuard( const FileGuard &copy )
+	{
+		fp= copy.fp;
+		refcount= copy.refcount;
+		++*refcount;
+	}
+
+	FileGuard &operator= ( const FileGuard &copy )
+	{
+		if( !--*refcount )
+		{
+			fclose( fp );
+			delete refcount;
+		}
+		fp= copy.fp;
+		refcount= copy.refcount;
+		++*refcount;
+
+		return *this;
+	}
+
+	~FileGuard()
+	{
+		if( !--*refcount )
+		{
+			fclose( fp );
+			delete refcount;
+		}
+	}
+
+	// Returns true if there is no file open, or
+	// if there is an error on the file pointer itself.
+	bool error() { return !fp || ferror( fp ); }
+
+	// Returns true when a file is at the end (or no file is open.)
+	bool eof() { return !fp || feof( fp ); }
+
+	// Copy data from the file into the specified buffer (and update
+	// its size).  Returns true when an error in reading has occurred
+	// and false otherwise.
+	bool
+	read( MemoryGuard *guard )
+	{
+		if( !fp ) return true;
+		size_t amount= guard->amount;
+		guard->amount= fread( guard->p, 1, amount, fp );
+		return guard->amount < amount && ( error() || !eof() );
+	}
+
+	// Copy data from the specified buffer into this file.  Returns true
+	// when an error in writing has occurred and false otherwise.
+	bool
+	write( const MemoryGuard *guard )
+	{
+		if( !fp ) return true;
+		size_t amount= fwrite( guard->p, 1, guard->amount, fp );
+		return amount < guard->amount || error();
+	}
+};
+
+
 /*
  * MemoryGuard can be combined with an error code, to facilitate returning
  * from functions and reporting errors.
@@ -232,7 +255,7 @@ main( int argcnt, char *argvec[] )
 		MemoryGuard buf= bufWithStatus.memory;
 		if( DEBUG_MODE ) fprintf( stderr, "Read %zu bytes\n", buf.amount );
 
-		if( ( fwrite( buf.p, 1, buf.amount, outfile.fp ) < buf.amount ) && outfile.error() )
+		if( outfile.write( &buf ) )
 		{
 			fprintf( stderr, "An error in copying occurred.\n" );
 			return -1;
@@ -253,17 +276,10 @@ read_buffer( size_t amount, FileGuard infile )
 	if( DEBUG_MODE ) fprintf( stderr, "Try to allocate %zu bytes\n", amount );
 	MemoryGuard internal( amount );
 	if( internal.p == NULL ) return MemoryGuardReturn( -2 );
-
 	if( DEBUG_MODE ) fprintf( stderr, "allocated %zu bytes\n", amount );
 
-	// This is only safe to do, because `fread` never returns more than the amount.
-	internal.amount= fread( internal.p, 1, amount, infile.fp );
+	if( infile.read( &internal ) ) return MemoryGuardReturn( -1 );
 	if( DEBUG_MODE ) fprintf( stderr, "just read %zu bytes\n", internal.amount );
-
-	if( internal.amount < COPYBUFSIZE && ( infile.error() || !infile.eof() ) )
-	{
-		return MemoryGuardReturn( -1 );
-	}
 
 	// Transfer the ownership to the caller
 	return MemoryGuardReturn( internal );
